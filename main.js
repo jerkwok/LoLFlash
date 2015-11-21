83cae5bbc680ab0a84cbb1ad922aa80d1cdbc33e
akiraaida <akira.aida@live.com>
2015-11-21 14:50:23 -0500

    Commented out line

--- main.js
+++ main.js
@@ -276,7 +276,7 @@ function displayGame(match){
 	//DOESN'T QUITE WORK YET BUT ALMOST THERE.
 	//NEED TO GO TO WORK
 
-	document.getElementById("content").innerHTML += "<img src=http://ddragon.leagueoflegends.com/cdn/5.22.3/img/champion/" + champion + ".png></img>"
+	//document.getElementById("content").innerHTML += "<img src=http://ddragon.leagueoflegends.com/cdn/5.22.3/img/champion/" + champion + ".png></img>"
 	// document.getElementById("content").innerHTML += " Match Id:" + match.matchId
 
 }
