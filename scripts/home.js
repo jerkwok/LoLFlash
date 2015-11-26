1e2b5e9d473d8317da16443caa69556dc36363fd
jerkwok <jeremy.kwok@uoit.net>
2015-11-26 12:54:33 -0500

    clicking the table works

--- scripts/home.js
+++ scripts/home.js
@@ -74,6 +74,8 @@ $(document).ready(function(){
 
 function buildTable(responsedata){
 
+	regionMap = getregionMap();
+	seasonMap = getseasonMap();
 	newTable = document.createElement("table");
 	newTable.setAttribute("class", "table table-inverse");
 	newBody = document.createElement("tbody");
@@ -88,20 +90,21 @@ function buildTable(responsedata){
 			var rowID = "results_row_" + i
 			newRow.setAttribute("id",rowID)
 			
-			for(j = 0; j < 5; j++){
+			for(j = 1; j < 5; j++){
 				newCol = document.createElement("td");
 				text = document.createTextNode("")
 
-				if(j == 0){
-					text.nodeValue = i+1
-
-				} else if(j == 1){
+				if(j == 1){
+					//username
 					text.nodeValue = responsedata[i][0]
 				} else if(j == 2){
-					text.nodeValue = responsedata[i][1]
+					//region
+					text.nodeValue = regionMap[responsedata[i][1]]
 				} else if(j == 3){
-					text.nodeValue = responsedata[i][2]
+					//season
+					text.nodeValue = seasonMap[responsedata[i][2]]
 				} else if(j == 4){
+					//history
 					text.nodeValue = responsedata[i][3]
 				}
 
@@ -118,11 +121,69 @@ function buildTable(responsedata){
 }
 
 function rowClick(row){
+
+	RregionMap = getRregionMap();
+	RseasonMap = getRseasonMap();
+
+	console.log(RseasonMap)
+
 	rowID = "results_row_" + row
 	var rowdata = document.getElementById(rowID).childNodes
-	window.location = './'+ rowdata[4].textContent +
-				'.html?username=' + rowdata[1].textContent +
-				'&region=' + rowdata[2].textContent + 
-				'&season=' + rowdata[3].textContent;
+	window.location = './'+ rowdata[3].textContent +
+				'.html?username=' + rowdata[0].textContent +
+				'&region=' + RregionMap[rowdata[1].textContent] + 
+				'&season=' + RseasonMap[rowdata[2].textContent];
 	// console.log(rowID)
+}
+
+function getregionMap(){
+	var regionMap = new Object();
+
+	regionMap["na"]="North America"
+ 	regionMap["euw"]="Europe West"
+ 	regionMap["eune"]="Europe Nordic & East"
+ 	regionMap["br"]="Brazil"
+ 	regionMap["kr"]="Korea"
+ 	regionMap["tr"]="Turkey"
+ 	regionMap["ru"]="Russia"
+ 	regionMap["lan"]="Latin America North"
+ 	regionMap["las"]="Latin America South"
+ 	regionMap["oce"]="Oceania"
+
+ 	return regionMap
+}
+
+function getseasonMap(){
+	var seasonMap = new Object();
+
+ 	seasonMap["PRESEASON3"]="Preseason 3"
+    seasonMap["SEASON3"]="Season 3"
+    seasonMap["PRESEASON2014"]="Preseason 4"
+    seasonMap["SEASON2014"]="Season 4"
+    seasonMap["PRESEASON2015"]="Preseason 5"
+    seasonMap["SEASON2015"]="Season 5"
+    seasonMap["PRESEASON2016"]="Preseason 6"
+
+    return seasonMap
+}
+
+function getRregionMap(){
+
+    var RregionMap = new Object();
+    regionMap = getregionMap();
+    for(i in regionMap){
+    	RregionMap[regionMap[i]] = i
+    }
+
+    return RregionMap
+}
+
+function getRseasonMap(){
+	var RseasonMap = new Object();
+	seasonMap = getseasonMap();
+    for(i in seasonMap){
+    	RseasonMap[seasonMap[i]] = i
+    }
+
+    return RseasonMap
 }
