36b354b5e4c599205608e55ffc9be8b46446d50a
akiraaida <akira.aida@live.com>
2015-11-25 23:07:04 -0500

    Started work on leagues. Got data

---
+++ scripts/leagues.js
@@ -0,0 +1,107 @@
+$(document).ready(function(){
+
+	var parameters = location.search.substring(1).split("&");
+	
+	if(parameters != ""){
+		var splitParams = parameters[0].split("=");
+
+		var username = unescape(parameters[0].split("=")[1])
+		var region = parameters[1].split("=")[1]
+		var queue = parameters[2].split("=")[1]
+
+		getID(username, region, queue);
+	}
+
+	$("#goButton").click(function(){
+
+		var username = $("#username").val()
+		var region = $('#region').val()
+		var season = $('#season').val()
+		// season not used in this page. May remove after
+
+		getID(username, region, season);
+	});
+
+	$("#clearButton").click(function(){
+		
+	});
+});
+
+function clear(id){
+	document.getElementById(id).innerHTML = ""
+}
+
+function getID(user, region, season){
+
+	var summonerUrl = "https://" + region + ".api.pvp.net/api/lol/" + region + "/v1.4/summoner/by-name/" + user + "?api_key=a45ee173-8cd1-4345-955c-c06a8ae10bec"
+	$.ajax({
+		url: summonerUrl,
+		type: 'GET',
+		dataType: 'json',
+		success: function(data){
+			user = user.replace(" ", "");
+			user = user.toLowerCase().trim();
+
+			sID = data[user].id;
+
+			getLeagues(region, sID);
+		}
+	})
+}
+
+function getLeagues(region, sID){
+	
+	var leagueUrl = "https://" + region + ".api.pvp.net/api/lol/" + region + "/v2.5/league/by-summoner/" + sID + "?api_key=a45ee173-8cd1-4345-955c-c06a8ae10bec"
+
+	$.ajax({
+		url: leagueUrl,
+		type: 'GET',
+		dataType: 'json',
+		success: function(data){
+
+			for(i = 0; i < data[sID].length; i++){
+
+				if(data[sID][i].queue == "RANKED_SOLO_5x5"){
+					
+					//console.log(data)
+
+					var divisionList = [];
+
+					// Find the division the summoner is in
+					for(j = 0; j < data[sID][i].entries.length; j++){
+						
+						if(sID == data[sID][i].entries[j].playerOrTeamId){
+							division = data[sID][i].entries[j].division
+						}
+					}
+
+					// Push all the summoners of the same divison into the divisionList
+					for(j = 0; j < data[sID][i].entries.length; j++){
+						
+						if(division == data[sID][i].entries[j].division){
+							divisionList.push(data[sID][i].entries[j])
+						}
+					}
+
+					// Sorts the divison list
+					sortByPoints(divisionList)
+
+					// Prints to the html
+					for(j = 0; j < divisionList.length; j++){
+						document.getElementById("resultstablediv").innerHTML += "<p>" + divisionList[j].playerOrTeamName + " - " + divisionList[j].leaguePoints + " LP</p>";
+					}
+					break;
+				}
+
+			}
+
+		}
+	})
+}
+
+function sortByPoints(list){
+	list.sort(function(a, b){
+    	return a.leaguePoints - b.leaguePoints;
+	});
+}
+
